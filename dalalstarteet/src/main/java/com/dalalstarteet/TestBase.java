f7a7cbb20a5f534409274374db62e29ab2b502d8
shraddha <navalshraddha@gmail.com>
2022-12-19 13:37:46 +0530

    donee

---
+++ dalalstarteet/src/main/java/com/dalalstarteet/TestBase.java
@@ -0,0 +1,5 @@
+package com.dalalstarteet;
+
+public class TestBase {
+
+}
---
+++ dalalstarteet/src/test/java/dalalstarteet/dalalstarteet/AppTest.java
@@ -0,0 +1,20 @@
+package dalalstarteet.dalalstarteet;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
